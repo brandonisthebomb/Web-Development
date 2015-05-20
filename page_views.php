a12d4495efbbc0df1cafe6f89f12da18b7d3c657
brandonisthebomb <liu.b97@gmail.com>
2015-05-20 11:37:19 -0400

    v1.5

---
+++ page_views.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Page Views</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="Styles/index.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+
+    <style>
+
+    </style>
+</head>
+
+
+<body>
+    <?php include ('tracking.php');?>
+
+    <h1 class="title-default">Page Views</h1>
+</body>
+
+</html>
